0425e6368ed8c31c73c235cb3941edccbadf5231
Ben L <39037350+blawson69@users.noreply.github.com>
2020-01-14 19:47:48 -0800

    Update to 1.2
    Added support for custom token markers; Added ability to change locked token marker; Modified lock command to add token marker to newly locked token if others are showing the marker.

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # TokenLocker
 
+> **New in v1.1:** TokenLocker has been updated to support the new [custom token markers](https://wiki.roll20.net/Custom_Token_Markers).
+
 This [Roll20](http://roll20.net/) script allows the GM to lock the position and/or the rotation of selected tokens.
+
+A Locked token marker can be toggled on and off to show which tokens are locked ([see below](#--show)). This defaults to the *padlock* but may be changed to any token marker desired. The Help Menu provides a "Choose Marker" button to display all token markers *including custom token markers* for easy selection, or you can use the "set manually" link to provide the name or name::ID combo for any valid token markers.
+
+Be careful of conflicts with other scripts, such as [EncumbranceTracker](https://github.com/blawson69/EncumbranceTracker), [ExhaustionTracker](https://github.com/blawson69/ExhaustionTracker), [CombatTracker](https://github.com/vicberg/Combattracker), and others that also modify token markers. If changing the Locked token marker, **make sure you choose a token marker that is not being used by another script.**
 
 ## Syntax
 
@@ -15,7 +21,7 @@ This [Roll20](http://roll20.net/) script allows the GM to lock the position and/
 
 ---
 ### --help
-Whispers a help dialog in the chat window with all the command options.
+Whispers a help dialog in the chat window with all the command options and a "Choose Marker" button to select a token marker other than the default.
 
 ```
 !tl --help
@@ -48,7 +54,7 @@ This command will unlock both the position and the rotation of all selected toke
 
 ---
 ### --show
-When you have a number of tokens locked, you may forget which ones they are. For this reason, the  `--show` command will place a _padlock_ status marker on every token that is currently locked in some way.
+When you have a number of tokens locked, you may forget which ones they are. For this reason, the  `--show` command will place a Locked token marker on every token that is currently locked in some way. This applies _across all pages_, so keep that in mind if you are in-game.
 
 ```
 !tl --show
@@ -56,7 +62,7 @@ When you have a number of tokens locked, you may forget which ones they are. For
 
 ---
 ### --hide
-This removes the  _padlock_ status marker from all locked tokens.
+This removes the Locked token marker from all locked tokens.
 ```
 !tl --hide
 ```
--- TokenLocker.js
+++ TokenLocker.js
@@ -4,7 +4,9 @@ locks the position and/or rotation of selected tokens.
 
 On Github:	https://github.com/blawson69
 Contact me: https://app.roll20.net/users/1781274/ben-l
-Like this script? Buy me a coffee: https://venmo.com/theRealBenLawson
+
+Like this script? Become a patron:
+    https://www.patreon.com/benscripts
 */
 
 var TokenLocker = TokenLocker || (function () {
@@ -12,11 +14,15 @@ var TokenLocker = TokenLocker || (function () {
 
     //---- INFO ----//
 
-    var version = '1.1',
+    var version = '1.2',
+    debugMode = false,
+    MARKERS,
+    ALT_MARKERS = [{name:'red', tag: 'red', url:"#C91010"}, {name: 'blue', tag: 'blue', url: "#1076C9"}, {name: 'green', tag: 'green', url: "#2FC910"}, {name: 'brown', tag: 'brown', url: "#C97310"}, {name: 'purple', tag: 'purple', url: "#9510C9"}, {name: 'pink', tag: 'pink', url: "#EB75E1"}, {name: 'yellow', tag: 'yellow', url: "#E5EB75"}, {name: 'dead', tag: 'dead', url: "X"}],
     styles = {
         box:  'background-color: #fff; border: 1px solid #000; padding: 8px 10px; border-radius: 6px; margin-left: -40px; margin-right: 0px;',
         title: 'padding: 0 0 10px 0; color: ##591209; font-size: 1.5em; font-weight: bold; font-variant: small-caps; font-family: "Times New Roman",Times,serif;',
         button: 'background-color: #000; border-width: 0px; border-radius: 5px; padding: 5px 8px; color: #fff; text-align: center;',
+        buttonWrapper: 'text-align: center; margin: 10px 0; clear: both;',
         textButton: 'background-color: transparent; border: none; padding: 0; color: #591209; text-decoration: underline;',
         code: 'font-family: "Courier New", Courier, monospace; background-color: #ddd; color: #000; padding: 2px 4px;',
     },
@@ -24,7 +30,14 @@ var TokenLocker = TokenLocker || (function () {
     checkInstall = function() {
         if (!_.has(state, 'TokenLocker')) state['TokenLocker'] = state['TokenLocker'] || {};
         if (typeof state['TokenLocker'].lockedTokens == 'undefined') state['TokenLocker'].lockedTokens = {};
+        if (typeof state['TokenLocker'].showing == 'undefined') state['TokenLocker'].showing = false;
+        if (typeof state['TokenLocker'].marker == 'undefined') state['TokenLocker'].marker = 'padlock';
+        MARKERS = JSON.parse(Campaign().get("token_markers"));
         log('--> TokenLocker v' + version + ' <-- Initialized. You currently have ' + _.size(state['TokenLocker'].lockedTokens) + ' token(s) locked.');
+        if (debugMode) {
+			var d = new Date();
+			showDialog('Debug Mode', 'TokenLocker v' + version + ' loaded at ' + d.toLocaleTimeString() + '<br><a style=\'' + styles.textButton + '\' href="!tl config">Show help</a>', 'GM');
+		}
     },
 
     //----- INPUT HANDLER -----//
@@ -55,6 +68,12 @@ var TokenLocker = TokenLocker || (function () {
                     case '--hide':
 						commandShowHide(msg, 'hide');
 						break;
+                    case 'markers':
+						commandShowMarkers(msg);
+						break;
+                    case 'set-marker':
+						commandSetMarker(msg, msg.content.split(/\s+/i).pop().toLowerCase());
+						break;
                     case '--help':
                     default:
                         commandHelp(msg);
@@ -80,9 +99,10 @@ var TokenLocker = TokenLocker || (function () {
             let token_id = token.get('id');
             if (action == 'unlock') {
                 lockedTokens = _.omit(lockedTokens, token_id);
-                token.set('status_padlock', false);
+                token.set('status_' + state['TokenLocker'].marker, false);
             } else {
                 lockedTokens[token_id] = {lockPosition: pos, lockRotation: rot};
+                token.set('status_' + state['TokenLocker'].marker, state['TokenLocker'].showing);
             }
 		});
 
@@ -100,36 +120,106 @@ var TokenLocker = TokenLocker || (function () {
 	},
 
     commandHelp = function(msg) {
-        var message = '<b>!tl --help</b><br>Sends this Help dialog to the chat window.<br><br>'
-        + '<b>!tl --lock</b><br>Locks the position <i>and</i> rotation of the selected token(s). Attempts <b>by anyone</b> to move or rotate '
-        + 'the token will result in a reset to the previous position/rotation.<br><br>'
-        + '<b>!tl --lock pos rot</b><br>Locks the position <i>and</i> rotation of the selected token(s) just like above. The "pos" and "rot" options can be in any order.<br><br>'
-        + '<b>!tl --lock pos</b><br>Locks <i>only</i> the position of the selected token(s). Rotation is still changable or will be unlocked.<br><br>'
-        + '<b>!tl --lock rot</b><br>Locks <i>only</i> the rotation of the selected token(s). Position is still changable or will be unlocked.<br><br>'
-        + '<b>!tl --unlock</b><br>Unlocks both the position and rotation of the selected token(s).<br><br>'
-        + '<b>!tl --show</b><br>Adds the <i>padlock</i> status marker to all locked tokens so they are easily recognizable. This <i>will</i> be visible to all players.<br><br>'
-        + '<b>!tl --hide</b><br>Removes the <i>padlock</i> status marker from all locked tokens.';
+        var marker_style = 'margin: 5px 10px 0 0; display: block; float: left;', message = '<h4>Commands</h4>';
+        message += '<a style="' + styles.textButton + ' font-weight: bold;" href="!tl --lock">!tl --lock</a><br>Locks the position <i>and</i> rotation of the selected token(s). Attempts <b>by anyone</b> to move or rotate ';
+        message += 'the token will result in a reset to the previous position/rotation.<br><br>';
+        message += '<a style="' + styles.textButton + ' font-weight: bold;" href="!tl --lock pos rot">!tl --lock pos rot</a><br>Locks the position <i>and</i> rotation of the selected token(s) just like above. The "pos" and "rot" options can be in any order.<br><br>';
+        message += '<a style="' + styles.textButton + ' font-weight: bold;" href="!tl --lock pos">!tl --lock pos</a><br>Locks <i>only</i> the position of the selected token(s). Rotation is still changable or will be unlocked.<br><br>';
+        message += '<a style="' + styles.textButton + ' font-weight: bold;" href="!tl --lock rot">!tl --lock rot</a><br>Locks <i>only</i> the rotation of the selected token(s). Position is still changable or will be unlocked.<br><br>';
+        message += '<a style="' + styles.textButton + ' font-weight: bold;" href="!tl --unlock">!tl --unlock</a><br>Unlocks both the position and rotation of the selected token(s).<br><br>';
+        message += '<a style="' + styles.textButton + ' font-weight: bold;" href="!tl --show">!tl --show</a><br>Adds the Locked token marker to all locked tokens so they are easily recognizable. This <i>will</i> be visible to all players.<br><br>';
+        message += '<a style="' + styles.textButton + ' font-weight: bold;" href="!tl --hide">!tl --hide</a><br>Removes the Locked token marker from all locked tokens but <i>does not</i> unlock them.';
+
+        var curr_marker = _.find(MARKERS, function (x) { return x.tag == state['TokenLocker'].marker; });
+        if (typeof curr_marker == 'undefined') curr_marker = _.find(ALT_MARKERS, function (x) { return x.tag == state['TokenLocker'].marker; });
+        message += '<hr><h4>Locked Token Marker</h4>' + getMarker(curr_marker.tag, marker_style) + 'This is the current Locked token marker. You may change it below.';
+        message += '<div style="' + styles.buttonWrapper + '"><a style="' + styles.button + '" href="!tl markers" title="This may result in a very long list...">Choose Marker</a></div>';
+        message += '<div style="text-align: center;"><a style="' + styles.textButton + '" href="!tl --set-marker &#63;&#123;Status Marker&#124;&#125;">Set manually</a></div>';
+
         showDialog('Help Menu', message);
     },
 
-	showDialog = function (title, content) {
+    commandShowHide = function(msg, action) {
+        // Places an indicator on locked tokens for ease of finding them, or removes the indicator
+        var lockedTokens = state['TokenLocker'].lockedTokens;
+        var show = (action == 'show') ? true : false;
+        state['TokenLocker'].showing = show;
+        _.each(state['TokenLocker'].lockedTokens, function(obj, id) {
+            var token = getObj('graphic', id);
+            if (token) {
+                token.set('status_' + state['TokenLocker'].marker, show);
+            }
+        });
+    },
+
+    commandSetMarker = function (msg, marker) {
+        marker = marker.replace('=', '::');
+        var status_markers = _.pluck(MARKERS, 'tag');
+        _.each(_.pluck(ALT_MARKERS, 'tag'), function (x) { status_markers.push(x); });
+        if (_.find(status_markers, function (tmp) {return tmp === marker; })) {
+            state['TokenLocker'].marker = marker;
+        } else {
+            showDialog('Error', 'The token marker "' + marker + '" is invalid. Please try again.');
+        }
+        commandHelp(msg);
+    },
+
+    commandShowMarkers = function () {
+        var message = '<table style="border: 0; width: 100%;" cellpadding="0" cellspacing="2">';
+        _.each(ALT_MARKERS, function (marker) {
+            message += '<tr><td>' + getMarker(marker.tag, 'margin-right: 10px;') + '</td><td style="white-space: nowrap; width: 100%;">' + marker.name + '</td>';
+            if (marker == state['TokenLocker'].marker) {
+                message += '<td style="text-align: center;">Current</td>';
+            } else {
+                message += '<td style="text-align: center; white-space: nowrap; padding: 7px;"><a style="' + styles.button + '" href="!tl set-marker ' + marker.tag + '">Set Marker</a></td>';
+            }
+            message += '</tr>';
+        });
+
+        _.each(MARKERS, function (icon) {
+            message += '<tr><td>' + getMarker(icon.tag, 'margin-right: 10px;') + '</td><td style="white-space: nowrap; width: 100%;">' + icon.name + '</td>';
+            if (icon.tag == state['TokenLocker'].marker) {
+                message += '<td style="text-align: center;">Current</td>';
+            } else {
+                message += '<td style="text-align: center; white-space: nowrap; padding: 7px;"><a style="' + styles.button + '" href="!tl set-marker ' + icon.tag.replace('::','=') + '">Set Marker</a></td>';
+            }
+            message += '</tr>';
+        });
+
+        message += '<tr><td colspan="3" style="text-align: center; padding: 7px;"><a style="' + styles.button + '" href="!tl --help">&#9668; Back</a></td></tr>';
+        message += '</table>';
+        showDialog('Choose Locked Marker', message);
+    },
+
+    getMarker = function (marker, style = '') {
+        var return_marker = '',
+        marker_style = 'width: 24px; height: 24px;' + style,
+        status_markers = _.pluck(MARKERS, 'tag'),
+        alt_marker = _.find(ALT_MARKERS, function (x) { return x.tag == marker; });
+
+        if (_.find(status_markers, function (x) { return x == marker; })) {
+            var icon = _.find(MARKERS, function (x) { return x.tag == marker; });
+            return_marker = '<img src="' + icon.url + '" width="24" height="24" style="' + marker_style + '" />';
+        } else if (typeof alt_marker !== 'undefined') {
+            if (alt_marker.url === 'X') {
+                marker_style += 'color: #C91010; font-size: 30px; line-height: 24px; font-weight: bold; text-align: center; padding-top: 0px; overflow: hidden;';
+                return_marker = '<div style="' + marker_style + '">X</div>';
+            } else {
+                marker_style += 'background-color: ' + alt_marker.url + '; border: 1px solid #fff; border-radius: 50%;';
+                return_marker = '<div style="' + marker_style + '"></div>';
+            }
+        } else {
+            return_marker = '<div style="' + marker_style + ' color: #c00;">--</div>';
+        }
+        return return_marker;
+    },
+
+    showDialog = function (title, content) {
         // Constructs a dialog box for chat output
         title = (title == '') ? '' : '<div style=\'' + styles.title + '\'>' + title + '</div>';
         var body = '<div style=\'' + styles.box + '\'>' + title + '<div>' + content + '</div></div>';
         sendChat('TokenLocker','/w GM ' + body, null, {noarchive:true});
 	},
-
-    commandShowHide = function(msg, action) {
-        // Places an indicator on locked tokens for ease of finding them, or removes the indicator
-        var lockedTokens = state['TokenLocker'].lockedTokens;
-        _.each(state['TokenLocker'].lockedTokens, function(obj, id) {
-            var token = getObj('graphic', id);
-            if (token) {
-                var show = (action == 'show') ? true : false;
-                token.set('status_padlock', show);
-            }
-        });
-    },
 
     handleMove = function(obj, prev) {
         // Enforces locks on tokens
